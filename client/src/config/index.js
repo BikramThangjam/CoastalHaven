a635cc282d6e1bd5a20e9fd05f69b5242a912388
BikramThangjam <tbsingh1211@gmail.com>
2024-05-04 14:42:05 +0530

    update API_URL to render.com server api

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,5 +1,6 @@
 const config = {
-    API_URL: "http://localhost:3001"
+    // API_URL: "http://localhost:3001",
+    API_URL: "https://coastalhaven-server.onrender.com"
 }
 
 export const {API_URL} = config;
